edbcf8538f83d8c57001eb4ee363c2799f98ad81
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-01 15:27:11 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 (function () {
-    $(document).ready(function () {
+  $(document).ready(function () {
+    // add item on button click //
+    $('#add').click(addItem);
+  });
 
-      }
-    })();
+  function addItem() {
+    var newItem = $('#textbox').val();
+    if (newItem.trim().length === 0) {
+      alert("You must enter an item to be added!");
+      return;
+    }
+    var listItem = createListItem(newItem);
+    $('#ulList').append(listItem);
+    $('#textbox').val('');
+  }
+
+  function createListItem(newItem) {
+    var listItem = "<li class='ui-state-default'><input type='checkbox'>";
+    listItem += "<span>" + newItem + "</span>";
+    listItem += "<a href='#'>remove</a></li>";
+    return listItem;
+  }
+})();
