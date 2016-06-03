d8f93dd4202e3961a71498bccba0f9a6cd733518
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-03 02:45:50 -0500

    design changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-(function () {
-  $(document).ready(function () {
-    // add item on button click //
-    $('#inputbox').click(function () {
-      alert($('#textbox').val());
-    });
+$(document).ready(function () {
+  // add item on button click //
+  $(document).on('submit', '#add', addListItem);
+  event.preventDefault();
 
-  });
-})();
+
+});
+
+function addListItem() {
+  var text = $('#textbox').val();
+  $('.i-need').append('<ul id="need_list"><li class="items">' + text + '</li></ul>');
+  $('#textbox').val('');
+}
