31b06cd25d484c3a7a62f01ba9bdc03602dfaf1a
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-05 16:46:39 -0500

    list items work

--- js/main.js
+++ js/main.js
@@ -3,11 +3,30 @@ $(document).ready(function () {
   $(document).on('click', '#add', addListItem);
   event.preventDefault();
 
+  //$('.delete').on('click', deleteListItem);
+  //$('.done').on('click', finishItem);
+  $(document).on('click', '.done', finishItem);
+  $(document).on('click', '.delete', deleteListItem);
+
 
 });
-
+//adds text from textbox
 function addListItem() {
   var text = $('#textbox').val();
-  $('.i-need').append('<ul id="need_list"><li class="items">' + text + '</li></ul>');
+  $('.i-need').append('<ul class="need_list"><li class="items"><input type="checkbox" class="done"/>' + text + '<button class="delete">x</button></li></ul>');
+  //sets textbox to empty once item is entered
   $('#textbox').val('');
+
+}
+//delete button works
+function deleteListItem() {
+  $(this).parent().remove();
+}
+//puts text decorations on checkbox
+function finishItem() {
+  if ($(this).parent().css('textDecoration') == 'line-through') {
+    $(this).parent().css('textDecoration', 'none').css('color', '#1ADB37');
+  } else {
+    $(this).parent().css('textDecoration', 'line-through').css('color', 'red');
+  }
 }
