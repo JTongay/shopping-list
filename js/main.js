688e3e6148ce95fbe8e790eeab2da77359064472
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-05 17:30:41 -0500

    fixed spacing issue

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 $(document).ready(function () {
   // add item on button click //
   $(document).on('click', '#add', addListItem);
+  /*
+  $('#textbox').keydown(function (event) {
+    var keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode === 13) {
+      addListItem();
+    }
+    This almost worked. The page just reloaded right after it showed without the new text added.
+  })
+  */
+  /*
+  $('#textbox').keydown(function (e) {
+    if (e.which === 13) {
+      addListItem();
+    }
+  })
+  same thing happened here
+  */
   event.preventDefault();
 
   //$('.delete').on('click', deleteListItem);
   //$('.done').on('click', finishItem);
   $(document).on('click', '.done', finishItem);
   $(document).on('click', '.delete', deleteListItem);
+
+
 
 
 });
@@ -17,7 +36,7 @@ function addListItem() {
     alert('You must enter an item.');
     return;
   }
-  $('.i-need').append('<ul class="need_list"><li class="items"><input type="checkbox" class="done"/>' + text + '<button class="delete">x</button></li></ul>');
+  $('.i-need').append('<ul class="need_list"><li class="items"><input type="checkbox" class="done"/><span>  ' + text + '</span><button class="delete">x</button></li></ul>');
   //sets textbox to empty once item is entered
   $('#textbox').val('');
 
