115d262b71f3a38334ab7481ba167a065b1f895c
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-13 13:11:00 -0500

    add iffy

--- js/main.js
+++ js/main.js
@@ -1,60 +1,62 @@
-$(function () {
-  // add item on button click //
-  $(document)
-    .on('click', '#add', addListItem)
-    .on('keydown', '#textbox', addWithEnterKey)
-    .on('click', '.done', finishItem)
-    .on('click', '.delete', deleteListItem);
-  /*
-  $('#textbox').keydown(function (event) {
-    var keycode = event.keyCode ? event.keyCode : event.which;
-    if (keycode === 13) {
-      addListItem();
+(function () {
+  $(function () {
+    // add item on button click //
+    $(document)
+      .on('click', '#add', addListItem)
+      .on('keydown', '#textbox', addWithEnterKey)
+      .on('click', '.done', finishItem)
+      .on('click', '.delete', deleteListItem);
+    /*
+    $('#textbox').keydown(function (event) {
+      var keycode = event.keyCode ? event.keyCode : event.which;
+      if (keycode === 13) {
+        addListItem();
+      }
+      This almost worked. The page just reloaded right after it showed without the new text added.
+    })
+    */
+
+
+    //$('.delete').on('click', deleteListItem);
+    //$('.done').on('click', finishItem);
+
+
+
+
+
+  });
+  //adds text from textbox
+  function addListItem() {
+    event.preventDefault();
+    var text = $('#textbox').val();
+    if (text.trim().length === 0) {
+      alert('You must enter an item.');
+      return;
     }
-    This almost worked. The page just reloaded right after it showed without the new text added.
-  })
-  */
+    $('.i-need').append('<ul class="need_list"><li class="items"><input type="checkbox" class="done"/><span>  ' + text + '</span><button class="delete">x</button></li></ul>');
+    //sets textbox to empty once item is entered
+    $('#textbox').val('');
 
 
-  //$('.delete').on('click', deleteListItem);
-  //$('.done').on('click', finishItem);
+  }
 
-
-
-
-
+  function addWithEnterKey(event) {
+    //uses enter key to work as well
+    if (event.keyCode === 13) {
+      addListItem(event);
+    }
+  }
+  //delete button works
+  function deleteListItem() {
+    $(this).parent().remove();
+  }
+  //puts text decorations on checkbox
+  function finishItem() {
+    event.preventDefault();
+    if ($(this).parent().css('textDecoration') == 'line-through') {
+      $(this).parent().css('textDecoration', 'none').css('color', '#1ADB37');
+    } else {
+      $(this).parent().css('textDecoration', 'line-through').css('color', '#EE816E');
+    }
+  }
 });
-//adds text from textbox
-function addListItem() {
-  event.preventDefault();
-  var text = $('#textbox').val();
-  if (text.trim().length === 0) {
-    alert('You must enter an item.');
-    return;
-  }
-  $('.i-need').append('<ul class="need_list"><li class="items"><input type="checkbox" class="done"/><span>  ' + text + '</span><button class="delete">x</button></li></ul>');
-  //sets textbox to empty once item is entered
-  $('#textbox').val('');
-
-
-}
-
-function addWithEnterKey(event) {
-  //uses enter key to work as well
-  if (event.keyCode === 13) {
-    addListItem(event);
-  }
-}
-//delete button works
-function deleteListItem() {
-  $(this).parent().remove();
-}
-//puts text decorations on checkbox
-function finishItem() {
-  event.preventDefault();
-  if ($(this).parent().css('textDecoration') == 'line-through') {
-    $(this).parent().css('textDecoration', 'none').css('color', '#1ADB37');
-  } else {
-    $(this).parent().css('textDecoration', 'line-through').css('color', '#EE816E');
-  }
-}
